3c86c836bbc2c2285bc0d9b2df654e8d18ee51bb
Matthew Docherty <mattdocherty314@gmail.com>
2018-09-26 16:41:36 +1000

    Edit Data Button fills Edit Section

--- frontend/scripts/edit-computers.js
+++ frontend/scripts/edit-computers.js
@@ -5,6 +5,8 @@ function pageLoad() {
     var comJSON = loadComputerJSON();
 
     displayComputerName(comDiv, comJSON);
+
+    addEventListeners(comJSON);
 }
 
 function loadComputerJSON() {
@@ -26,4 +28,72 @@ function displayComputerName(div, json) {
         div.innerHTML += "<button id='edit-com-"+c+"'> Edit Data </button>";
         div.innerHTML += "</div><br>";
     }
+}
+
+function addEventListeners(json) {
+    var clearBtn = document.getElementById("clear");
+    clearBtn.addEventListener("click", clearData);
+
+    var modifyBtn = document.getElementById("modify");
+    modifyBtn.addEventListener("click", modifyData);
+
+    var i = 0;
+    do {
+        var currentEditButton = document.getElementById("edit-com-"+i);
+        currentEditButton.addEventListener("click", (evt) => editScript(evt.srcElement.id.split("-")[2], json));
+        i += 1;
+    } while (currentEditButton !== null);
+}
+
+function editScript(id, json) {
+    var startStopBtn = document.getElementById("start-stop");
+    var editName = document.getElementById("edit-name");
+    var editOS = document.getElementById("edit-os");
+    var editIP = document.getElementById("edit-ip");
+    var editPort = document.getElementById("edit-port");
+    var editUser = document.getElementById("edit-user");
+    var editPass = document.getElementById("edit-pass");
+    var modifyBtn = document.getElementById("modify");
+
+    editName.setAttribute("value", json[id].name);
+    editOS.setAttribute("value", json[id].os);
+    editIP.setAttribute("value", json[id].ip);
+    editPort.setAttribute("value", json[id].port);
+    editUser.setAttribute("value", json[id].user);
+    editPass.setAttribute("value", json[id].pass);
+    modifyBtn.innerHTML = "Edit Computer";
+    startStopBtn.disabled = false;
+    if (json[id].online === true) {
+        startStopBtn.innerHTML = "Stop Server";
+    } else {
+        startStopBtn.innerHTML = "Start Server";
+    }
+}
+
+function clearData() {
+    var startStopBtn = document.getElementById("start-stop");
+    var editName = document.getElementById("edit-name");
+    var editOS = document.getElementById("edit-os");
+    var editIP = document.getElementById("edit-ip");
+    var editPort = document.getElementById("edit-port");
+    var editUser = document.getElementById("edit-user");
+    var editPass = document.getElementById("edit-pass");
+    var modifyBtn = document.getElementById("modify");
+
+    editName.setAttribute("value", "");
+    editOS.setAttribute("value", "");
+    editIP.setAttribute("value", "");
+    editPort.setAttribute("value", "");
+    editUser.setAttribute("value", "");
+    editPass.setAttribute("value", "");
+    modifyBtn.innerHTML = "Add Computer";
+    startStopBtn.disabled = true;
+    startStopBtn.innerHTML = "Start Server";
+}
+
+function modifyData() {
+    //sendModifcation();
+}
+
+function sendModifcation(type, data) {
 }
