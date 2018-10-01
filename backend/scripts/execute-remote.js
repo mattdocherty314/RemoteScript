17731acae810704b4f4dda1d8efb7f9787dc5b7b
Matthew Docherty <mattdocherty314@gmail.com>
2018-10-01 18:49:54 +1000

    Setup for Executing Remote Scripts

--- backend/scripts/execute-remote.js
+++ backend/scripts/execute-remote.js
@@ -1,6 +1,52 @@
+var exec = require("child_process").exec;
+var fs = require("fs");
+
 exports.execute_remote = function(computer, script) {
     main(computer, script);
 }
 
 function main(computer, script) {
+    var computerSettings = getComputerSettings(computer);
+    var scriptSettings = getScriptSettings(script);
+
+    startScript(computerSettings, scriptSettings);
+}
+
+function getComputerSettings(computer) {
+    var computerDB = fs.readFileSync("backend/json/computers.json");
+    var computerJSON = JSON.parse(computerDB);
+    for (var c in computerJSON) {
+        if (computerJSON[c].name === computer) {
+            var computerData = computerJSON[c];
+            break;
+        }
+    }
+
+    return computerData;
+}
+
+function getScriptSettings(script) {
+    var scriptDB = fs.readFileSync("backend/json/scripts.json");
+    var scriptJSON = JSON.parse(scriptDB);
+    for (var s in scriptJSON) {
+        if (scriptJSON[s].name === script) {
+            var scriptData = scriptJSON[s];
+            break;
+        }
+    }
+
+    return scriptData;
+}
+
+function startScript(computerData, scriptData) {
+    var script = scriptData.content;
+    
+    var ip = computerData.ip;
+    var port = computerData.port;
+    var user = computerData.user;
+    var pass = computerData.pass;
+
+    exec(command, function(err, stdout, stderr) {
+        console.log(stdout);
+    });
 }
