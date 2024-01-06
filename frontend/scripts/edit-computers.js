d00e4d69c74409bf16003c3aa3a994b1689f6ce3
BuildTools <unconfigured@null.spigotmc.org>
2024-01-06 21:08:27 +1000

    Added functionality to the 'Add/Modify Computer' button

--- frontend/scripts/edit-computers.js
+++ frontend/scripts/edit-computers.js
@@ -27,6 +27,7 @@ function selectComputer(itemID) {
 		editPassField.value = res[0].pass;
 	})
 	.catch((err) => {
+		let errorDiv = document.getElementById("error");
 		errorDiv.innerHTML = err;
 	})
 }
@@ -52,9 +53,40 @@ function clearFields() {
 	modifyButton.innerHTML = "Add Computer";
 }
 
+function submitChanges() {
+	let editNameField = document.getElementById("edit-name");
+	let editOSField = document.getElementById("edit-os");
+	let editIPField = document.getElementById("edit-ip");
+	let editPortField = document.getElementById("edit-port");
+	let editUserField = document.getElementById("edit-user");
+	let editPassField = document.getElementById("edit-pass");
+
+	let submitEntry = {
+		"name": editNameField.value,
+		"os": editOSField.value,
+		"ip": editIPField.value,
+		"port": parseInt(editPortField.value),
+		"user": editUserField.value,
+		"pass": editPassField.value
+	}
+	fetch(`${HOST}/edit-computer/${selectedItemID}`, {
+		method: "POST",
+		body: JSON.stringify(submitEntry)
+	})
+	.then((res) => {
+		return res.json();
+	})
+	.then((res) => {
+		console.log(res)
+	})
+	.catch((err) => {
+		let errorDiv = document.getElementById("error");
+		errorDiv.innerHTML = err;
+	})
+}
+
 function pageLoad() {
 	let listComputersDiv = document.getElementById("list-computers");
-	let errorDiv = document.getElementById("error");
 	fetch(`${HOST}/view-computers`)
 	.then((res) => {
 		return res.json();
@@ -67,11 +99,15 @@ function pageLoad() {
 		});
 	})
 	.catch((err) => {
+		let errorDiv = document.getElementById("error");
 		errorDiv.innerHTML = err;
 	})
 
 	let clearButton = document.getElementById("clear");
 	clearButton.addEventListener("click", clearFields);
+
+	let submitButton = document.getElementById("modify");
+	submitButton.addEventListener("click", submitChanges);
 }
 
 
